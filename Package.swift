df3aa4e645b90c75ffea4c4755c6b6f99be27168
Martin Johannesson <mjohannesson@gmail.com>
2023-08-13 19:42:12 +0200

    Bump deployment target to iOS 15, macOS 12, tvOS 15

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
     name: "SQLiteKit",
     platforms: [
-        .macOS(.v10_14), .iOS(.v12), .tvOS(.v12)
+        .macOS(.v12), .iOS(.v15), .tvOS(.v15)
     ],
     products: [
         .library(name: "SQLiteKit", targets: ["SQLiteKit"])
